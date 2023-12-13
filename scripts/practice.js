ee849c3696a9902993063948d808354763129a3a
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2023-12-12 19:33:59 -0800

    variables

---
+++ scripts/practice.js
@@ -0,0 +1,40 @@
+console.log("Practice");
+
+// professor's info
+let professorName="Samantha";
+let myGrade=4.0;
+let isProfessor = true;
+const URL ="https://sdgku.edu";
+
+professorName="Darius";
+professorName="Samuel";
+professorName="David";
+//display the values
+console.log("My name is -  " + professorName);
+console.log("FSDI 101 grade: " + myGrade);
+console.log("Active: " +isProfessor);
+// student's info
+
+let num1=10;
+let num2=5;
+
+console.log("Multiplication: " + num1*num2);
+console.log("Addition: " + (num1+num2));
+console.log("Sub: " + (num1-num2));
+console.log("Div: " + (num1/num2));
+
+
+let assignmentName = "Class 1 - Assingment 1";
+let description= "Assignments are due the day Session 3 is held by 11:59 p.m. (in the time zone in which each student resides). Assignments represent 20% of the overall course grade.";
+let pointsAs1 = 6.66;
+let pointsAs2 = 6.66;
+let pointsAs3 = 6.66;
+
+let total = pointsAs1 + pointsAs2 + pointsAs3;
+let fileUploads= true;
+let studentAnnotation = false;
+
+let dueDate="12/20/2023";
+
+document.write(`<h1> ${assignmentName} </h1>`);
+document.write(`<p> ${description} </p>`);
